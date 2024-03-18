944ed1ca0006c1ec62c536e97548ba7317a0e16f
Galizoo <gabriel_nov_005@hotmail.com>
2024-03-18 14:38:44 -0500

    Se agrego comentarios

--- inicio.py
+++ inicio.py
@@ -1,1 +1,22 @@
+#Comandos para la creacion de nuestro repocitorio en git hub 
+#git init                                   --para inicializar e repocitorio
+#git status                                 --para verificar que es lo que vamos a subir /no es obligatorio solo es para revisar el estado
+#git add                                    --(si solo queremos subir un archivo en especifico colocaremos el nombre) si se quiere colocar todo colocar --git add . 
+#git commit -m "Se agrega un proyecto"      --sirven para subir los cambios en repocitorio
+#Luego de esto nos pedira colocar nuestro usuario y correo de git
+#git config --global user.email (Correo)
+#git config --global user.name (Tu nombre de usuario)
+#Repetimos el: git commit -m "Se agrega un proyecto" 
+#Aqui solo tenemos el repocitorio en nuestro repocitorio remoto 
+#git branch -M main --sirve para indicar que vamos a subir esto a nuestra rama principal 
+#Copiamos la ruta especifica de --git remote al crear el repocitorio en el git hub
+#git push -u origin main --indicamos que vamos hacer el push para poder hacer efectiva la sincronisacion de mi local con mi remoto  
+#Luego de ello se te abrira una ventana que te pedira que confirmes el acceso de git hub y se subiran los archivos 
+#Y si no pararece eso en la consola se te pedira que te logues con tu usuario y contraseña 
+#Y eso es todo para poder crear y subir archivos en un repocitorio 
+
+
+#Ahora para poder hacer push los colaboradores a mi repocitorio
+#git clone (pegamos la url) --Clonaremos el codigo  del repocitorio
+#git push --si no tienes colaborador no te va a funcionar 
 print ("Holas, este sera el inicio de nuestro proyecto o intento de proyecto de chat bot UwU")
