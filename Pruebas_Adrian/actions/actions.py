3f22dcbc5d2aaecb8d07a9eae8ded534467a2b60
Maurice <20173374@aloe.ulima.edu.pe>
2024-04-16 12:01:54 -0500

    cambios en el modelo, español y otros detalles

--- Pruebas_Adrian/actions/actions.py
+++ Pruebas_Adrian/actions/actions.py
@@ -7,21 +7,18 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SessionStarted, ActionExecuted, EventType
+
+class ActionSessionStart(Action):
+    def name(self) -> str:
+        return "action_session_start"
+
+    async def run(self, dispatcher, tracker, domain):
+        # Aquí va tu lógica personalizada
+        # Ejemplo: enviar un mensaje de bienvenida
+        dispatcher.utter_message("¡Hola! Bienvenido al chatbot.")
+
+        return [SessionStarted(), ActionExecuted("action_listen")]
